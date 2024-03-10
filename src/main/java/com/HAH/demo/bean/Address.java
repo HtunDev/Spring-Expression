2bed0a0d427f219d986d35c00e23e20bc96e310e
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-10 17:53:18 +0630

    using record root object in expression

---
+++ src/main/java/com/HAH/demo/bean/Address.java
@@ -0,0 +1,5 @@
+package com.HAH.demo.bean;
+
+public record Address(String street, String building, String townShip) {
+
+}
--- src/test/java/com/HAH/demo/test/RootObjectTest.java
+++ src/test/java/com/HAH/demo/test/RootObjectTest.java
@@ -1,18 +1,30 @@
 package com.HAH.demo.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
+import com.HAH.demo.bean.Address;
 import com.HAH.demo.bean.Student;
 
 public class RootObjectTest {
 
+	ExpressionParser parser;
+
+	@BeforeEach
+	void gettingParser() {
+		parser = new SpelExpressionParser();
+	}
+
 	@Test
 	void rootTest() {
 		var student = new Student("Htun Aung Hlaing", 23, List.of("Java", "Spring", "Angular", "React"));
-		var parser = new SpelExpressionParser();
+
 		var expression = parser.parseExpression("name");
 		var expressName = (String) expression.getValue(student);
 //		var expressName = expression.getValue(student,String.class);
@@ -31,6 +43,25 @@ public class RootObjectTest {
 		var expressInterest1 = expression.getValue(student, String.class);
 		System.out.println(expressInterest1);
 
+	}
+
+	@Test
+	void recordTest() {
+		var address = new Address("San Pya", "120/F", "Pyi G Thakon");
+		var expression = parser.parseExpression("street");
+		var expressStreet = (String) expression.getValue(address);
+		System.out.println(expressStreet);
+		assertEquals(address.street(), expressStreet);
+
+		expression = parser.parseExpression("building");
+		var expressBuilding = (String) expression.getValue(address);
+		System.out.println(expressBuilding);
+		assertEquals(address.building(), expressBuilding);
+
+		expression = parser.parseExpression("townShip");
+		var expressTownShip = (String) expression.getValue(address);
+		System.out.println(expressTownShip);
+		assertEquals(address.townShip(), expressTownShip);
 	}
 
 }
